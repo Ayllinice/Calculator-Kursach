29706506d1f3df95fa63faf87909c9b6fcdddc5c
Ayllinice <m2209576@edu.misis.ru>
2023-06-01 01:33:10 +0300

    Добавьте файлы проекта.

---
+++ Calc/Form1.Designer.cs
@@ -0,0 +1,371 @@
+﻿namespace Calc
+{
+    partial class Calculator
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Calculator));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.protocolToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.but7 = new System.Windows.Forms.Button();
+            this.but8 = new System.Windows.Forms.Button();
+            this.but9 = new System.Windows.Forms.Button();
+            this.but4 = new System.Windows.Forms.Button();
+            this.but5 = new System.Windows.Forms.Button();
+            this.but6 = new System.Windows.Forms.Button();
+            this.but1 = new System.Windows.Forms.Button();
+            this.but2 = new System.Windows.Forms.Button();
+            this.but3 = new System.Windows.Forms.Button();
+            this.but0 = new System.Windows.Forms.Button();
+            this.butplus = new System.Windows.Forms.Button();
+            this.butequal = new System.Windows.Forms.Button();
+            this.butminus = new System.Windows.Forms.Button();
+            this.butmultiply = new System.Windows.Forms.Button();
+            this.butdivide = new System.Windows.Forms.Button();
+            this.butdelete = new System.Windows.Forms.Button();
+            this.butcom = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(28, 28);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(28, 28);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.protocolToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(601, 38);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // protocolToolStripMenuItem
+            // 
+            this.protocolToolStripMenuItem.Name = "protocolToolStripMenuItem";
+            this.protocolToolStripMenuItem.Size = new System.Drawing.Size(108, 34);
+            this.protocolToolStripMenuItem.Text = "Protocol";
+            this.protocolToolStripMenuItem.Click += new System.EventHandler(this.protocolToolStripMenuItem_Click);
+            // 
+            // but7
+            // 
+            this.but7.Font = new System.Drawing.Font("Segoe UI Emoji", 20.14286F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.but7.Location = new System.Drawing.Point(147, 351);
+            this.but7.Name = "but7";
+            this.but7.Size = new System.Drawing.Size(97, 79);
+            this.but7.TabIndex = 3;
+            this.but7.Text = "7";
+            this.but7.UseVisualStyleBackColor = true;
+            this.but7.Click += new System.EventHandler(this.NumberButton_Click);
+            // 
+            // but8
+            // 
+            this.but8.Font = new System.Drawing.Font("Segoe UI Emoji", 20.14286F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.but8.Location = new System.Drawing.Point(250, 351);
+            this.but8.Name = "but8";
+            this.but8.Size = new System.Drawing.Size(97, 79);
+            this.but8.TabIndex = 4;
+            this.but8.Text = "8";
+            this.but8.UseVisualStyleBackColor = true;
+            this.but8.Click += new System.EventHandler(this.NumberButton_Click);
+            // 
+            // but9
+            // 
+            this.but9.Font = new System.Drawing.Font("Segoe UI Emoji", 20.14286F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.but9.Location = new System.Drawing.Point(353, 351);
+            this.but9.Name = "but9";
+            this.but9.Size = new System.Drawing.Size(97, 79);
+            this.but9.TabIndex = 5;
+            this.but9.Text = "9";
+            this.but9.UseVisualStyleBackColor = true;
+            this.but9.Click += new System.EventHandler(this.NumberButton_Click);
+            // 
+            // but4
+            // 
+            this.but4.Font = new System.Drawing.Font("Segoe UI Emoji", 20.14286F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.but4.Location = new System.Drawing.Point(147, 436);
+            this.but4.Name = "but4";
+            this.but4.Size = new System.Drawing.Size(97, 79);
+            this.but4.TabIndex = 6;
+            this.but4.Text = "4";
+            this.but4.UseVisualStyleBackColor = true;
+            this.but4.Click += new System.EventHandler(this.NumberButton_Click);
+            // 
+            // but5
+            // 
+            this.but5.Font = new System.Drawing.Font("Segoe UI Emoji", 20.14286F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.but5.Location = new System.Drawing.Point(250, 436);
+            this.but5.Name = "but5";
+            this.but5.Size = new System.Drawing.Size(97, 79);
+            this.but5.TabIndex = 7;
+            this.but5.Text = "5";
+            this.but5.UseVisualStyleBackColor = true;
+            this.but5.Click += new System.EventHandler(this.NumberButton_Click);
+            // 
+            // but6
+            // 
+            this.but6.Font = new System.Drawing.Font("Segoe UI Emoji", 20.14286F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.but6.Location = new System.Drawing.Point(353, 436);
+            this.but6.Name = "but6";
+            this.but6.Size = new System.Drawing.Size(97, 79);
+            this.but6.TabIndex = 8;
+            this.but6.Text = "6";
+            this.but6.UseVisualStyleBackColor = true;
+            this.but6.Click += new System.EventHandler(this.NumberButton_Click);
+            // 
+            // but1
+            // 
+            this.but1.Font = new System.Drawing.Font("Segoe UI Emoji", 20.14286F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.but1.Location = new System.Drawing.Point(147, 521);
+            this.but1.Name = "but1";
+            this.but1.Size = new System.Drawing.Size(97, 79);
+            this.but1.TabIndex = 9;
+            this.but1.Text = "1";
+            this.but1.UseVisualStyleBackColor = true;
+            this.but1.Click += new System.EventHandler(this.NumberButton_Click);
+            // 
+            // but2
+            // 
+            this.but2.Font = new System.Drawing.Font("Segoe UI Emoji", 20.14286F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.but2.Location = new System.Drawing.Point(250, 521);
+            this.but2.Name = "but2";
+            this.but2.Size = new System.Drawing.Size(97, 79);
+            this.but2.TabIndex = 10;
+            this.but2.Text = "2";
+            this.but2.UseVisualStyleBackColor = true;
+            this.but2.Click += new System.EventHandler(this.NumberButton_Click);
+            // 
+            // but3
+            // 
+            this.but3.Font = new System.Drawing.Font("Segoe UI Emoji", 20.14286F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.but3.Location = new System.Drawing.Point(353, 521);
+            this.but3.Name = "but3";
+            this.but3.Size = new System.Drawing.Size(97, 79);
+            this.but3.TabIndex = 11;
+            this.but3.Text = "3";
+            this.but3.UseVisualStyleBackColor = true;
+            this.but3.Click += new System.EventHandler(this.NumberButton_Click);
+            // 
+            // but0
+            // 
+            this.but0.Font = new System.Drawing.Font("Segoe UI Emoji", 20.14286F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.but0.Location = new System.Drawing.Point(250, 606);
+            this.but0.Name = "but0";
+            this.but0.Size = new System.Drawing.Size(97, 79);
+            this.but0.TabIndex = 12;
+            this.but0.Text = "0";
+            this.but0.UseVisualStyleBackColor = true;
+            this.but0.Click += new System.EventHandler(this.NumberButton_Click);
+            // 
+            // butplus
+            // 
+            this.butplus.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.butplus.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("butplus.BackgroundImage")));
+            this.butplus.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.butplus.Location = new System.Drawing.Point(456, 606);
+            this.butplus.Name = "butplus";
+            this.butplus.Size = new System.Drawing.Size(97, 79);
+            this.butplus.TabIndex = 13;
+            this.butplus.Text = "+";
+            this.butplus.UseVisualStyleBackColor = false;
+            this.butplus.Click += new System.EventHandler(this.OperatorButton_Click);
+            // 
+            // butequal
+            // 
+            this.butequal.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.butequal.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.butequal.Location = new System.Drawing.Point(147, 606);
+            this.butequal.Name = "butequal";
+            this.butequal.Size = new System.Drawing.Size(97, 79);
+            this.butequal.TabIndex = 14;
+            this.butequal.Text = "=";
+            this.butequal.UseVisualStyleBackColor = false;
+            this.butequal.Click += new System.EventHandler(this.EqualsButton_Click);
+            // 
+            // butminus
+            // 
+            this.butminus.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.butminus.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.butminus.Location = new System.Drawing.Point(456, 521);
+            this.butminus.Name = "butminus";
+            this.butminus.Size = new System.Drawing.Size(97, 79);
+            this.butminus.TabIndex = 15;
+            this.butminus.Text = "-";
+            this.butminus.UseVisualStyleBackColor = false;
+            this.butminus.Click += new System.EventHandler(this.OperatorButton_Click);
+            // 
+            // butmultiply
+            // 
+            this.butmultiply.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.butmultiply.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.butmultiply.Location = new System.Drawing.Point(456, 436);
+            this.butmultiply.Name = "butmultiply";
+            this.butmultiply.Size = new System.Drawing.Size(97, 79);
+            this.butmultiply.TabIndex = 16;
+            this.butmultiply.Text = "*";
+            this.butmultiply.UseVisualStyleBackColor = false;
+            this.butmultiply.Click += new System.EventHandler(this.OperatorButton_Click);
+            // 
+            // butdivide
+            // 
+            this.butdivide.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.butdivide.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.butdivide.Location = new System.Drawing.Point(456, 351);
+            this.butdivide.Name = "butdivide";
+            this.butdivide.Size = new System.Drawing.Size(97, 79);
+            this.butdivide.TabIndex = 17;
+            this.butdivide.Text = "/";
+            this.butdivide.UseVisualStyleBackColor = false;
+            this.butdivide.Click += new System.EventHandler(this.OperatorButton_Click);
+            // 
+            // butdelete
+            // 
+            this.butdelete.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.butdelete.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("butdelete.BackgroundImage")));
+            this.butdelete.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.butdelete.Location = new System.Drawing.Point(456, 266);
+            this.butdelete.Name = "butdelete";
+            this.butdelete.Size = new System.Drawing.Size(97, 79);
+            this.butdelete.TabIndex = 18;
+            this.butdelete.Text = " ";
+            this.butdelete.UseVisualStyleBackColor = false;
+            // 
+            // butcom
+            // 
+            this.butcom.BackColor = System.Drawing.SystemColors.Window;
+            this.butcom.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.butcom.Font = new System.Drawing.Font("Segoe UI Emoji", 20.14286F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.butcom.Location = new System.Drawing.Point(353, 606);
+            this.butcom.Name = "butcom";
+            this.butcom.Size = new System.Drawing.Size(97, 79);
+            this.butcom.TabIndex = 19;
+            this.butcom.Text = ".";
+            this.butcom.UseVisualStyleBackColor = false;
+            this.butcom.Click += new System.EventHandler(this.NumberButton_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Segoe UI Emoji", 40.14286F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.richTextBox1.Location = new System.Drawing.Point(47, 61);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(506, 168);
+            this.richTextBox1.TabIndex = 20;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI Emoji", 20.14286F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(353, 266);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 79);
+            this.button1.TabIndex = 21;
+            this.button1.Text = "C";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI Emoji", 20.14286F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(250, 266);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(97, 79);
+            this.button2.TabIndex = 22;
+            this.button2.Text = "CE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(601, 751);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.butcom);
+            this.Controls.Add(this.butdelete);
+            this.Controls.Add(this.butdivide);
+            this.Controls.Add(this.butmultiply);
+            this.Controls.Add(this.butminus);
+            this.Controls.Add(this.butequal);
+            this.Controls.Add(this.butplus);
+            this.Controls.Add(this.but0);
+            this.Controls.Add(this.but3);
+            this.Controls.Add(this.but2);
+            this.Controls.Add(this.but1);
+            this.Controls.Add(this.but6);
+            this.Controls.Add(this.but5);
+            this.Controls.Add(this.but4);
+            this.Controls.Add(this.but9);
+            this.Controls.Add(this.but8);
+            this.Controls.Add(this.but7);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Calculator";
+            this.Text = "Calculator";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private ContextMenuStrip contextMenuStrip1;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem protocolToolStripMenuItem;
+        private Button but7;
+        private Button but8;
+        private Button but9;
+        private Button but4;
+        private Button but5;
+        private Button but6;
+        private Button but1;
+        private Button but2;
+        private Button but3;
+        private Button but0;
+        private Button butplus;
+        private Button butequal;
+        private Button butminus;
+        private Button butmultiply;
+        private Button butdivide;
+        private Button butdelete;
+        private Button butcom;
+        private RichTextBox richTextBox1;
+        private Button button1;
+        private Button button2;
+    }
+}
---
+++ Calc/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System.Globalization;
+
+namespace Calc
+{
+    public partial class Calculator : Form
+    {
+        private string currentNumber = "";
+        private string operation = "";
+        private decimal result = 0;
+        private bool isDecimal = false;
+
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void NumberButton_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+
+            if (button.Text == ".")
+            {
+                if (!isDecimal)
+                {
+                    isDecimal = true;
+                    currentNumber += button.Text;
+                    richTextBox1.Text += button.Text;
+                }
+            }
+            else
+            {
+                currentNumber += button.Text;
+                richTextBox1.Text += button.Text;
+            }
+        }
+
+        private void OperatorButton_Click(object sender, EventArgs e)
+        {
+
+            //Button button = (Button)sender;
+            //operation = button.Text;
+            //result = decimal.Parse(currentNumber);
+            //currentNumber = "";
+            //richTextBox1.Text = richTextBox1.Text.TrimEnd() + " " + button.Text;
+            Button button = (Button)sender;
+            operation = button.Text;
+
+            if (isDecimal)
+            {
+                decimal.TryParse(currentNumber, NumberStyles.Float, CultureInfo.InvariantCulture, out result);
+                isDecimal = false;
+            }
+            else
+            {
+                decimal.TryParse(currentNumber, out result);
+            }
+
+            currentNumber = "";
+            richTextBox1.Text += button.Text;
+        }
+
+        private void EqualsButton_Click(object sender, EventArgs e)
+        {
+            decimal secondNumber;
+            if (!decimal.TryParse(currentNumber, NumberStyles.Float, CultureInfo.InvariantCulture, out secondNumber))
+            {
+                MessageBox.Show("Invalid input!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            string protocolEntry = $"{result} {operation} {secondNumber} = {result}\n";
+            
+            try
+            {
+                File.AppendAllText("protocol.txt", protocolEntry);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to write to protocol file: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            switch (operation)
+            {
+                case "+":
+                    result += secondNumber;
+                    break;
+                case "-":
+                    result -= secondNumber;
+                    break;
+                case "*":
+                    result *= secondNumber;
+                    break;
+                case "/":
+                    if (secondNumber != 0)
+                    {
+                        result /= secondNumber;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Cannot divide by zero!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        try
+                        {
+                            protocolEntry = $"↑ Cannot divide by zero!\n";
+                            File.AppendAllText("protocol.txt", protocolEntry);
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show($"Failed to write to protocol file: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                        return;
+                    }
+                    break;
+            }
+
+            richTextBox1.Text = result.ToString(CultureInfo.InvariantCulture);
+            currentNumber = result.ToString(CultureInfo.InvariantCulture);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            richTextBox1.ClearUndo();
+            result = 0;
+            currentNumber = "";
+            operation = "";
+            try
+            {
+                if (File.Exists("protocol.txt"))
+                {
+                    File.Delete("protocol.txt");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to delete protocol file: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void protocolToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string protocolContent = File.ReadAllText("protocol.txt");
+                MessageBox.Show(protocolContent, "Protocol", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to open protocol file: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            richTextBox1.ClearUndo();
+            result = 0;
+            currentNumber = "";
+            operation = "";
+        }
+    }
+}
